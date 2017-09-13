6f132cfc3b6ac7e967b72fb4e39be5efa7ab7118
Jason K. Moore <moorepants@gmail.com>
2017-09-12 18:17:50 -0700

    Added travis yml file and a notebook execution script.

---
+++ bin/execute_notebooks.sh
@@ -0,0 +1,4 @@
+for f in notebooks/*.ipynb; do
+  jupyter nbconvert --debug --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 --to=html $f
+  jupyter nbconvert --debug --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 --to=notebook $f
+done
