fbafef9c3fe7ebbb5e521ba2da5f45538d7b8648
Kenneth Lyons <ixjlyons@gmail.com>
2017-09-19 09:01:02 -0700

    Draft of notebook 01.

--- bin/build_notebooks.sh
+++ bin/build_notebooks.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
 for srcpath in notebook-src/*.rst
 do
 	filename=$(basename "$srcpath")
 	rst2ipynb $srcpath "notebooks/${filename%.*}.ipynb"
 done
+
+cp notebook-src/fig/* notebooks/fig/
