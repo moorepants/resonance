6248f7fd20515d564ede5c8d1ac2b19c240e5519
Jason K. Moore <moorepants@gmail.com>
2017-09-26 11:17:04 -0700

    Fixed unit tests.

--- resonance/tests/test_linear_systems.py
+++ resonance/tests/test_linear_systems.py
@@ -1,15 +1,15 @@
 import pytest
 
-from ..linear_systems import *
+from ..linear_systems import _ParametersDict, _MeasurementsDict
 
 
 def test_nonvalid_parameters_key():
-    p = ParametersDict({})
+    p = _ParametersDict({})
     with pytest.raises(ValueError):
         p['ben stiller'] = 12.0
 
 
 def test_setting_measurements_item():
-    m = MeasurementsDict({})
+    m = _MeasurementsDict({})
     with pytest.raises(ValueError):
         m['a'] = 12.0
