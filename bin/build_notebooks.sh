07ff59c52f5034e6d5e99eb1ba556833a3ed6f99
Jason K. Moore <moorepants@gmail.com>
2017-09-26 18:21:46 -0700

    Change all matplotlib notebook backend calls with inline calls.

--- bin/build_notebooks.sh
+++ bin/build_notebooks.sh
@@ -5,4 +5,9 @@ do
 	filename=$(basename "$srcpath")
 	rst2ipynb "$srcpath" "notebooks/${filename%.*}.ipynb"
 done
+# %matplotlib notebook will not cause plots to render, so swap it.
+if [ "$TRAVIS" = "true" ]
+then
+	sed -i -- 's/%matplotlib notebook/%matplotlib inline/g' "notebooks/*.ipynb"
+fi
 cp notebook-src/fig/* notebooks/fig/
