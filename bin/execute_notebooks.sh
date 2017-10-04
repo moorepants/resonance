244eae8cc7b2355e5ed6a8ef30047b21e9d2a3f2
Jason K. Moore <moorepants@gmail.com>
2017-10-04 14:08:21 -0700

    Enabled the exercise2 nbextension for NB 03.

--- bin/execute_notebooks.sh
+++ bin/execute_notebooks.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+jupyter nbextension enable exercise2/main
+jupyter nbextension enable rubberband/main
+
 for f in notebooks/*.ipynb; do
   jupyter nbconvert --debug --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 --to=html "$f"
   jupyter nbconvert --debug --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 --to=notebook "$f"
