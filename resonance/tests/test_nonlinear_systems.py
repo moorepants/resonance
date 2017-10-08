f8ebbb044b0813912d99bc7374f3565545087a2d
Jason K. Moore <moorepants@gmail.com>
2017-10-08 14:25:38 -0700

    Explicit sample rate.

--- resonance/tests/test_nonlinear_systems.py
+++ resonance/tests/test_nonlinear_systems.py
@@ -67,11 +67,12 @@ def total_energy(kinetic_energy, potential_energy):
 
 sys.add_measurement('total_energy', total_energy)
 
-trajectories = sys.free_response(5.0)
+sample_rate = 30
+trajectories = sys.free_response(5.0, sample_rate=sample_rate)
 
 
-def plot_config(mass, length, sway, height, time__hist,
-                potential_energy__hist):
+def plot_config(mass, length, sway, height, time, sway__hist, height__hist,
+                time__hist, potential_energy__hist):
     fig, axes = plt.subplots(1, 2)
 
     circle_radius = mass / 2.0 / 10.0
@@ -80,14 +81,15 @@ def plot_config(mass, length, sway, height, time__hist,
     axes[0].set_ylim((-length - circle_radius, length + circle_radius))
     axes[0].set_title('Pendulum')
     axes[0].set_aspect('equal')
+    xlabel = axes[0].set_xlabel('Time: {:.3f}'.format(time))
 
+    path_lines = axes[0].plot(sway__hist, height__hist, color='red')
     rod_lines = axes[0].plot([0, sway], [0, height])
 
     circle = Circle((sway, height), radius=circle_radius)
-
     axes[0].add_patch(circle)
 
-    axes[1].set_ylim((0, 0.5))
+    axes[1].set_ylim((0, 0.4))
     axes[1].set_xlim((0, 5))
     axes[1].set_xlabel('Time [s]')
     axes[1].set_ylabel('Potential Energy [J]')
@@ -95,15 +97,20 @@ def plot_config(mass, length, sway, height, time__hist,
 
     plt.tight_layout()
 
-    return fig, circle, rod_lines, pe_lines
+    return fig, circle, rod_lines, path_lines, pe_lines, xlabel
 
 sys.config_plot_func = plot_config
 
 
-def update_plot(sway, height, time__hist, potential_energy__hist, circle,
-                rod_lines, pe_lines):
+def update_plot(sway, height, time, time__hist, sway__hist, height__hist,
+                potential_energy__hist, circle, rod_lines, path_lines,
+                pe_lines, xlabel):
     circle.center = sway, height
     rod_lines[0].set_data([0, sway], [0, height])
+    path_lines[0].set_data(sway__hist, height__hist)
     pe_lines[0].set_data(time__hist, potential_energy__hist)
+    xlabel.set_text('Time: {:.3f}'.format(time))
+
+    return circle, rod_lines, path_lines, pe_lines, xlabel
 
 sys.config_plot_update_func = update_plot
