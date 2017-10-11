9d0091088600a01737db6d96d915ace9c1db37ca
Jason K. Moore <moorepants@gmail.com>
2017-10-10 20:46:17 -0700

    Some docstrings.

--- resonance/nonlinear_systems.py
+++ resonance/nonlinear_systems.py
@@ -22,19 +22,23 @@ class NonLinearSystem(_System):
         return integrator_method(initial_conditions, times)
 
     def _integrate_with_lsoda(self, initial_conditions, times):
+        """This method should return the integration results in the form of
+        odeint."""
 
-        f = self.equations_of_motion
+        rhs = self.equations_of_motion
 
-        # the first two args will always be the state and then time
-        args = tuple([self._get_par_vals(k) for k in getargspec(f).args[2:]])
+        # NOTE: the first two args will always be the state and then time
+        args = tuple([self._get_par_vals(k) for k in getargspec(rhs).args[2:]])
 
-        return sp.integrate.odeint(f, initial_conditions, times, args=args)
+        return sp.integrate.odeint(rhs, initial_conditions, times, args=args)
 
     def _generate_state_trajectories(self, times):
+        """This method should return arrays for position, velocity, and
+        acceleration of the coordinates."""
         int_res = self._integrate_equations_of_motion(times)
         f = self.equations_of_motion
         args = tuple([self._get_par_vals(k) for k in getargspec(f).args[2:]])
-        res = np.array(f(int_res.T, times, *args))
+        res = np.asarray(f(int_res.T, times, *args))
         return int_res[:, 0], int_res[:, 1], res[1, :]
 
 
