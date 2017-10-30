c43cc17a59c64959f71aa0ea00158d1e05bb6e72
Kenneth Lyons <ixjlyons@gmail.com>
2017-10-29 21:02:24 -0700

    Remove use of rst2ipynb.

--- bin/execute_notebooks.sh
+++ bin/execute_notebooks.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -xe
 
+# %matplotlib notebook will not cause plots to render in html output, so swap it.
+if [ "$TRAVIS" = "true" ]
+then
+	echo "hello"
+	sed -i -- 's/%matplotlib notebook/%matplotlib inline/g' notebooks/*.ipynb
+fi
+
 for f in notebooks/*.ipynb; do
   jupyter nbconvert --debug --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 --to=html "$f"
   jupyter nbconvert --debug --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 --to=notebook "$f"
