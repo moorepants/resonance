59e0f3a068e6baa27998a1508294c2516791a5dd
Kenneth Lyons <ixjlyons@gmail.com>
2017-11-24 15:32:49 -0800

    Just execute notebooks once.

--- bin/execute_notebooks.sh
+++ bin/execute_notebooks.sh
@@ -9,5 +9,4 @@ fi
 
 for f in notebooks/*.ipynb; do
   jupyter nbconvert --debug --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 --to=html "$f"
-  jupyter nbconvert --debug --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 --to=notebook "$f"
 done
