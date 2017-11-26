ef25a07bf8e8d23f1179f9946250f728748290ad
Kenneth Lyons <ixjlyons@gmail.com>
2017-11-26 09:32:26 -0800

    Don't need to overwrite un-executed ipynb files.

--- bin/prepare_deploy.sh
+++ bin/prepare_deploy.sh
@@ -3,7 +3,3 @@ set -x
 mkdir deploy
 cp -R notebooks/* deploy/
 ls -R deploy
-# rename `<notebook>.nbconvert.ipynb` to `<notebook>.ipynb`
-for f in deploy/*.nbconvert.ipynb; do
-    mv "$f" "${f%.nbconvert.ipynb}.ipynb"
-done
